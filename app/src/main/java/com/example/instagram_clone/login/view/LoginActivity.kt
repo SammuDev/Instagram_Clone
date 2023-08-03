51cba6e1312934bd1e8f523c5ac6e335054cdf07
SammuDev <samucas878@gmail.com>
2023-08-03 12:07:00 -0300

    feat: Adicionando figuras para tela inicial de 'login' e definindo uma paleta de cores

--- app/src/main/java/com/example/instagram_clone/login/view/LoginActivity.kt
+++ app/src/main/java/com/example/instagram_clone/login/view/LoginActivity.kt
@@ -1,9 +1,10 @@
-package com.example.instagram_clone
+package com.example.instagram_clone.login.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.instagram_clone.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
