c53b7f4729068a7c8b07f29e9d664c856855e7a8
SammuDev <samucas878@gmail.com>
2023-08-08 11:58:03 -0300

    feat: Adicionando dinamicamente opções de seleção no 'Dialog' da foto do perfil

--- app/src/main/java/com/example/instagram_clone/common/view/CustomDialog.kt
+++ app/src/main/java/com/example/instagram_clone/common/view/CustomDialog.kt
@@ -2,6 +2,42 @@ package com.example.instagram_clone.common.view
 
 import android.app.Dialog
 import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.example.instagram_clone.R
 
 class CustomDialog(context: Context) : Dialog(context) {
+    private lateinit var dialogLinearLayout: LinearLayout
+    private lateinit var textsButton: Array<TextView>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dialog_custom)
+
+        dialogLinearLayout = findViewById(R.id.dialog_container)
+    }
+
+    fun addButton(listener: View.OnClickListener, vararg texts: Int) {
+        textsButton = Array(texts.size) {
+            TextView(context)
+        }
+
+        texts.forEachIndexed { idx, text ->
+            textsButton[idx].setText(text)
+            textsButton[idx].setOnClickListener {
+                listener.onClick(it)
+                dismiss()
+            }
+        }
+    }
+
+    override fun show() {
+        super.show()
+
+        for (textView in textsButton) {
+            dialogLinearLayout.addView(textView)
+        }
+    }
 }
