5727874a2ead7e3a85ed57711530ac028b8012d3
SammuDev <samucas878@gmail.com>
2023-08-08 07:42:30 -0300

    feat: Criando nova 'Activity' de 'Register'

---
+++ app/src/main/java/com/example/instagram_clone/register/view/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.instagram_clone.register.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.instagram_clone.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
