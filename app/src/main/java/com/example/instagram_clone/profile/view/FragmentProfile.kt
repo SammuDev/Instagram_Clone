ae95ff84bf84a98d096e13d0f98251cc2d6212b2
SammuDev <samucas878@gmail.com>
2023-09-01 17:02:40 -0300

    feat: Adicionando dinamicamente uma imagem teste para renderizar na página de perfil do usuário

--- app/src/main/java/com/example/instagram_clone/profile/view/FragmentProfile.kt
+++ app/src/main/java/com/example/instagram_clone/profile/view/FragmentProfile.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.instagram_clone.R
+import java.util.zip.Inflater
 
 class FragmentProfile : Fragment() {
     override fun onCreateView(
@@ -14,5 +18,40 @@ class FragmentProfile : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.profile_recyclerView)
+        recyclerView.layoutManager = GridLayoutManager(requireContext(), 3)
+        recyclerView.adapter = PostAdapter()
+    }
+
+    private class PostAdapter : RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): PostAdapter.PostViewHolder {
+            return PostViewHolder(
+                LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_profile_grid, parent, false)
+            )
+        }
+
+        override fun onBindViewHolder(holder: PostAdapter.PostViewHolder, position: Int) {
+//            holder.bind(R.drawable.ic_insta_add)
+            holder.bind(R.drawable.Kotlin_Logo_RGB)
+        }
+
+        override fun getItemCount(): Int {
+            return 30
+        }
+
+        private inner class PostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            fun bind(image: Int) {
+                itemView.findViewById<ImageView>(R.id.item_profile_image_grid).setImageResource(image)
+            }
+        }
     }
 }
