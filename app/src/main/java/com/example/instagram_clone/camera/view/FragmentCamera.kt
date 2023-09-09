347ba931b45b61f5e1699d7d028bef6c3ce170d3
SammuDev <samucas878@gmail.com>
2023-09-08 22:35:01 -0300

    feat: Criando novo fragment - inicialmente representando a visualização da câmera

---
+++ app/src/main/java/com/example/instagram_clone/camera/view/FragmentCamera.kt
@@ -0,0 +1,23 @@
+package com.example.instagram_clone.camera.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.instagram_clone.R
+
+class FragmentCamera : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
+}
