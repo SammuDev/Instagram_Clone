a074166ed021b0ba22f098afc894f70638924394
SammuDev <samucas878@gmail.com>
2023-09-18 17:20:37 -0300

    feat: activity_register.xml -> Removendo 'fragment' e adicionando 'FrameLayout'
    feat: RegisterActivity.kt -> Inflando através do 'supportFragmentManager' o 'FrameLayout'

--- app/src/main/java/com/example/instagram_clone/register/view/RegisterActivity.kt
+++ app/src/main/java/com/example/instagram_clone/register/view/RegisterActivity.kt
@@ -1,12 +1,24 @@
 package com.example.instagram_clone.register.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
 import com.example.instagram_clone.R
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ViewBinding
+
+    @SuppressLint("CommitTransaction")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val fragment = RegisterEmailFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.register_fragment, fragment)
+            commit()
+        }
     }
 }
