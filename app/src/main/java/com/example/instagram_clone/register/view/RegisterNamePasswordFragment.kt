5b39b077747d4d2638ac1331bd35a648aadf8b78
SammuDev <samucas878@gmail.com>
2023-09-22 18:10:16 -0300

    feat: Implementando os métodos para criação de 'name' e 'password' do usuário

--- app/src/main/java/com/example/instagram_clone/register/view/RegisterNamePasswordFragment.kt
+++ app/src/main/java/com/example/instagram_clone/register/view/RegisterNamePasswordFragment.kt
@@ -2,13 +2,18 @@ package com.example.instagram_clone.register.view
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.instagram_clone.R
 import com.example.instagram_clone.databinding.FragmentRegisterNamePasswordBinding
+import com.example.instagram_clone.register.RegisterNameAndPassword
 
-class RegisterNamePasswordFragment : Fragment(R.layout.fragment_register_name_password) {
+class RegisterNamePasswordFragment : Fragment(R.layout.fragment_register_name_password),
+    RegisterNameAndPassword.View {
 
     private var binding: FragmentRegisterNamePasswordBinding? = null
+
+    override lateinit var presenter: RegisterNameAndPassword.Presenter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -17,10 +22,31 @@ class RegisterNamePasswordFragment : Fragment(R.layout.fragment_register_name_pa
 
     override fun onDestroy() {
         binding = null
+        presenter.onDestroy()
         super.onDestroy()
     }
 
     companion object {
         const val KEY_EMAIL = "key_email"
+    }
+
+    override fun showProgress(enabled: Boolean) {
+        binding?.registerNameButtonNext?.showProgress(enabled)
+    }
+
+    override fun displayNameFailure(nameError: Int?) {
+        binding?.registerEditTextName?.error = nameError?.let { getString(it) }
+    }
+
+    override fun displayPasswordFailure(passError: Int?) {
+        binding?.registerEditTextPassword?.error = passError?.let { getString(it) }
+    }
+
+    override fun onCreateSuccess(name: String) {
+//        TODO: Abrir tela de bem vindo
+    }
+
+    override fun onCreateFailure(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
     }
 }
