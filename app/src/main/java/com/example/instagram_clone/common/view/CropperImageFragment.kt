b71915b0cd3b5f29f138f0a9b5e5058999e2b782
SammuDev <samucas878@gmail.com>
2023-09-27 23:54:44 -0300

    feat: inflando 'fragment' para fazer recorte de imagem

--- app/src/main/java/com/example/instagram_clone/common/view/CropperImageFragment.kt
+++ app/src/main/java/com/example/instagram_clone/common/view/CropperImageFragment.kt
@@ -1,7 +1,27 @@
 package com.example.instagram_clone.common.view
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.instagram_clone.R
+import com.example.instagram_clone.databinding.FragmentImageCropperBinding
 
 class CropperImageFragment : Fragment(R.layout.fragment_image_cropper) {
+    private var binding: FragmentImageCropperBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentImageCropperBinding.bind(view)
+
+        binding?.let {
+            with(it) {
+//                a
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
+    }
 }
