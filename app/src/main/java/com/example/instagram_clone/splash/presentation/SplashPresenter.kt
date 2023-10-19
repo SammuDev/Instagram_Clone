7e390fb48f0cdecd99fabf8af4a2d2adc5e3bbd9
SammuDev <samucas878@gmail.com>
2023-10-19 13:53:22 -0300

    feat: Direcionando usuário a uma tela específica (LoginScreen or MainScreen) após autenticação

--- app/src/main/java/com/example/instagram_clone/splash/presentation/SplashPresenter.kt
+++ app/src/main/java/com/example/instagram_clone/splash/presentation/SplashPresenter.kt
@@ -1,12 +1,21 @@
 package com.example.instagram_clone.splash.presentation
 
 import com.example.instagram_clone.splash.Splash
+import com.example.instagram_clone.splash.data.SplashCallback
 import com.example.instagram_clone.splash.data.SplashRepository
 
 class SplashPresenter(private var view: Splash.View?, private val repository: SplashRepository) :
     Splash.Presenter {
     override fun authenticated() {
-        TODO("Not yet implemented")
+        repository.session(object : SplashCallback {
+            override fun onSuccess() {
+                view?.goToMainScreen()
+            }
+
+            override fun onFailure() {
+                view?.goToLoginScreen()
+            }
+        })
     }
 
     override fun onDestroy() {
